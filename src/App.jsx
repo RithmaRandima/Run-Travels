744948cd1dc8d81e054db7f4808eab49c23b86bd
RithmaRandima <rithma.r.nandasiri.com>
2025-06-02 15:25:39 +0530

    Top Bar Creation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Topbar from "./components/Topbar";
+
+const App = () => {
+  return (
+    <div className="bg-red-400">
+      App
+      <Topbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Topbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BsChatDots } from "react-icons/bs";
+
+const Topbar = () => {
+  return (
+    <div>
+      <div>
+        <BsChatDots />
+        <h1>RUN travels</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
