cd9c308fb48858123ebe1a867b843a9ccfbedaf7
RithmaRandima <rithma.r.nandasiri.com>
2025-06-03 12:19:21 +0530

    Completed Gallery Section

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Booking = () => {
   return (
-    <div id="deals" className="max-w-[1140px] m-auto w-full p-4">
+    <div id="deals" className="max-w-[1140px] m-auto  w-full p-4">
       <form className="lg:flex lg:justify-between w-full items-center">
         <div className="flex flex-col my-2 py-2">
           <label>Destination</label>
-          <select className="lg:w[300px] md:w-full border rounded-md p-2">
+          <select className="lg:w-[300px] md:w-full border rounded-md p-2">
             <option>Grand Antigua</option>
             <option>Key West</option>
             <option>Maldives</option>
